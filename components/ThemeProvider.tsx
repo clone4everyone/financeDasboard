e9cd044506791e720ae3317c1e5c5af6c060ef70
clone4everyone <karanrawat9149@gmail.com>
2025-08-14 11:52:03 +0530

    fixes theme provider error

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,8 +1,20 @@
 "use client";
 
+import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+
+// Derive the prop types from the component (safe across versions)
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
 }
